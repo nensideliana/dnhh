ff80d370b2959c62b54681febf53023ee2c5cdba
Nensi Deliana <nensi.deliana@mail.utoronto.ca>
2019-05-18 19:04:25 -0400

    Found first instance of title-artist

--- script.py
+++ script.py
@@ -1,7 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 content = requests.get('https://www.hotnewhiphop.com/songs/').content
 soup = BeautifulSoup(content, "html.parser")
 
-print(soup.div)
+title = soup.select('.cover-title')[0].get_text().strip()
+artists  = soup.select('.grid-item-artists')[0].get_text().strip()
+artists = artists.replace('\xa0',' ')
+songs = {}
+songs[title] = artists
+print(title)
+print(artists)
+print(songs)
