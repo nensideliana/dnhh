f989191b06f6f4ff9e4c3d82e420a99b32a15f1a
Nensi Deliana <nensi.deliana@mail.utoronto.ca>
2019-05-20 18:02:55 -0400

    Added main artist check

--- script.py
+++ script.py
@@ -27,11 +27,9 @@ songs = {}
 for i in range(len(title_list)):
     title = title_list[i].get_text().strip()
     artist = artists_list[i].get_text().strip().replace('\xa0',' ')
-    songs[title] = artist
+    main_artist = artists_list[i].find('em').get_text().strip()
+    songs[title] = [artist,main_artist]
 
-t = list(songs.keys())[0]
-a = list(songs.values())[0]
-print(t,a)
 
 #get token to access spotify data
 scope='playlist-modify-private'
@@ -46,16 +44,13 @@ if token:
     playlists = sp.user_playlist_create(username,'DNHH '+ date,public=False)
     playlist_id = playlists['id']
 
-    #To check if playlist exists <<IN PROGRESS>>
-    # user_playlists = sp.user_playlists(username)
-    # print(user_playlists['items'][0]['name'])
-
     tracks = []
 
     #search for tracks in dictionary and add track id's to playlist
     for title in songs.keys():
 
-        artist = songs[title]
+        artist = songs[title][0]
+        main_artist = songs[title][1]
         results = sp.search(q='track:' + title + ' ' + artist, type='track')
 
         #if there are search results
@@ -72,6 +67,24 @@ if token:
             print('id:',search_id)
 
             tracks.append(search_id)
+        
+        #if no results try again with only the main artist
+        else:
+            results = sp.search(q='track:' + title + ' ' + main_artist, type='track')
+            
+            if(any(results) and results['tracks']['total'] != 0):
+          
+                #get first result
+                item = results['tracks']['items'][0]
+
+                search_title = item['name']
+                print('title:',search_title)
+                search_artist = item['artists'][0]['name']
+                print('artist:',search_artist)
+                search_id = item['id']
+                print('id:',search_id)
+
+                tracks.append(search_id)
 
     #add all songs to the playlist
     sp.user_playlist_add_tracks(username, playlist_id, tracks, position=None)
